f835c1bc17745b1c5bf16cc1137fe4889b9d9cdb
wangkuanzzu <wangkuan.zzu@qq.com>
2021-06-04 15:52:52 +0800

    外观模式

---
+++ src/main/java/designpattern/facade/TestMain.java
@@ -0,0 +1,31 @@
+package designpattern.facade;
+
+/**
+ * description: TestMain <br>
+ * date: 2021/6/4 上午10:06 <br>
+ * author: kuan <br>
+ * version: 1.0 <br>
+ */
+public class TestMain {
+
+    public static void main(String[] args) {
+
+        /*
+            外观模式
+            将一些复杂的接口进行整理合并
+            然后对外提供更少的接口
+            实现更多的功能
+         */
+
+        Computer computer = new Computer();
+        Game game = new Game();
+        Cocacola cocacola = new Cocacola();
+
+        PlayComputerGame playComputerGame = new PlayComputerGame(computer,game,cocacola);
+        playComputerGame.play();
+
+        playComputerGame.over();
+
+    }
+
+}
