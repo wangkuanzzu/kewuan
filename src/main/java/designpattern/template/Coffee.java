3aea819116502b6d97fcad872606a8162bb9a929
wangkuanzzu <wangkuan.zzu@qq.com>
2021-06-07 10:55:45 +0800

    模版方法 钩子方法

---
+++ src/main/java/designpattern/template/Coffee.java
@@ -0,0 +1,20 @@
+package designpattern.template;
+
+/**
+ * description: Caffee <br>
+ * date: 2021/6/7 上午10:33 <br>
+ * author: kuan <br>
+ * version: 1.0 <br>
+ */
+public class Coffee extends CaffeineBeverage {
+
+    @Override
+    void brew() {
+        System.out.println(" =打开一包咖啡= ");
+    }
+
+    @Override
+    void addCondiments() {
+        System.out.println(" =添加牛奶、白砂糖，变得甜一点= ");
+    }
+}
